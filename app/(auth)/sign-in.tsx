1fc350c998e41dc0e980b09ae1e42ee72ea2213a
Kaung Myat Han <kaungmyathan2222@gmail.com>
2024-10-28 14:48:53 +0630

    feat(): init files setup

---
+++ app/(auth)/sign-in.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import { SafeAreaView } from "react-native-safe-area-context";
+import { Text } from "react-native";
+
+const SignIn = () => {
+  return (
+    <SafeAreaView>
+      <Text>SignIn</Text>
+    </SafeAreaView>
+  );
+};
+export default SignIn;
